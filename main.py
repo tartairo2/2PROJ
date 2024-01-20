2c95f7b48b5b7ac94be80f5d9f295f394444a037
TARTAIRO <72879269+TARTAIRO@users.noreply.github.com>
2024-01-20 22:48:57 +0100

    Ajout d'une image de fond d'écran

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
+#import
 import pygame
-
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+WIDTH = 800
+HEIGHT = 600
+fps = 30
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Age of empire")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Age of War gam")
+timer = pygame.time.Clock()
+main_menu = False
+font = pygame.font.SysFont('freesansbold.ttf', 24)
 
-# Chargez l'image de fond
-background_image = pygame.image.load("bg.jpeg")  # Assurez-vous de mettre le bon nom de fichier
+def draw_menu():
+    pass
 
-def draw_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x, y))
-
+def draw_game():
+    menu_btn = pygame.draw.rect(screen, "light gray", [230, 450, 260, 40],0, 5)
+    pygame.draw.rect(screen, "dark gray", [230, 450, 260, 40], 5, 5)
+    text = font.render("Main menu ", True, "black")
+    screen.blit(text, (245, 457))
 run = True
 while run:
-    # Dessinez l'image de fond
-    screen.blit(background_image, (0, 0))
+    screen.fill("light blue")
+    timer.tick(fps)
+    if main_menu:
+        draw_menu()
+    else:
+        draw_game()
 
-    for event in pygame.event.get():
+    for event in pygame.event.get() :
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
-
-# Déplacez pygame.quit() en dehors de la boucle while
+    pygame.display.flip()
 pygame.quit()
